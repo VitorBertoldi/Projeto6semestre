e3fc7cd469f362716e280ec72fcfe1504e3de447
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2024-09-16 21:31:42 -0300

    singleton

--- classes/dispositivo.js
+++ classes/dispositivo.js
@@ -1,15 +1,68 @@
 class Dispositivo {
     constructor(id_dispositivo, id_usuario, nome_dispositivo, tipo_dispositivo, data_sincronizacao) {
-        this.id_dispositivo     = id_dispositivo
-        this.id_usuario         = id_usuario 
-        this.nome_dispositivo   = nome_dispositivo
-        this.tipo_dispositivo   = tipo_dispositivo
-        this.data_sincronizacao = data_sincronizacao
+        if (Dispositivo.instance) {
+            return Dispositivo.instance; // Retorna a instância existente se já foi criada
+        }
+
+        this.id_dispositivo     = id_dispositivo;
+        this.id_usuario         = id_usuario;
+        this.nome_dispositivo   = nome_dispositivo;
+        this.tipo_dispositivo   = tipo_dispositivo;
+        this.data_sincronizacao = data_sincronizacao;
+
+        Dispositivo.instance = this; // Guarda a instância para garantir que seja única
     }
-    
-    criarDispositivo(idUsuario, nomeDispositivo, tipoDispositivo){}
-    editarDispositivo(idDispositivo, nomeDispositivo, tipoDispositivo){}
-    excluirDispositivo(idDispositivo){}
-    getDispositivo(idDispositivo){}
-    getDispositivos(idUsuario){}
+
+    // Simulando um "banco de dados" em memória
+    static dispositivosDB = [];
+
+    static getInstance() {
+        if (!Dispositivo.instance) {
+            Dispositivo.instance = new Dispositivo();
+        }
+        return Dispositivo.instance;
+    }
+
+    criarDispositivo(idUsuario, nomeDispositivo, tipoDispositivo) {
+        const idDispositivo = Dispositivo.dispositivosDB.length + 1;
+        const dataSincronizacao = new Date().toISOString();
+        
+        const novoDispositivo = new Dispositivo(
+            idDispositivo, 
+            idUsuario, 
+            nomeDispositivo, 
+            tipoDispositivo, 
+            dataSincronizacao
+        );
+        
+        Dispositivo.dispositivosDB.push(novoDispositivo);
+        return novoDispositivo;
+    }
+
+    editarDispositivo(idDispositivo, nomeDispositivo, tipoDispositivo) {
+        const dispositivo = Dispositivo.dispositivosDB.find(d => d.id_dispositivo === idDispositivo);
+        if (dispositivo) {
+            dispositivo.nome_dispositivo = nomeDispositivo;
+            dispositivo.tipo_dispositivo = tipoDispositivo;
+            dispositivo.data_sincronizacao = new Date().toISOString();
+            return dispositivo;
+        }
+        return null; 
+    }
+
+    excluirDispositivo(idDispositivo) {
+        const index = Dispositivo.dispositivosDB.findIndex(d => d.id_dispositivo === idDispositivo);
+        if (index !== -1) {
+            return Dispositivo.dispositivosDB.splice(index, 1);
+        }
+        return null; 
+    }
+
+    getDispositivo(idDispositivo) {
+        return Dispositivo.dispositivosDB.find(d => d.id_dispositivo === idDispositivo) || null;
+    }
+
+    getDispositivos(idUsuario) {
+        return Dispositivo.dispositivosDB.filter(d => d.id_usuario === idUsuario);
+    }
 }
--- model/db.js
+++ model/db.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 
+class MongoDBConnection {
+  constructor() {
+    if (!MongoDBConnection.instance) {
+      this._connect();
+      MongoDBConnection.instance = this;
+    }
 
-mongoose.connect('mongodb+srv://vitorbertoldi:wrtYXEn67mrZT4im@cluster0.ad7ah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Conectado ao MongoDB');
-}).catch(err => {
-  console.error('Erro ao conectar ao MongoDB:', err);
-});
+    return MongoDBConnection.instance;
+  }
 
-const usuarioSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
-});
+  _connect() {
+    mongoose.connect('mongodb+srv://vitorbertoldi:wrtYXEn67mrZT4im@cluster0.ad7ah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+      .then(() => {
+        console.log('Conectado ao MongoDB');
+      })
+      .catch(err => {
+        console.error('Erro ao conectar ao MongoDB:', err);
+      });
+  }
+}
 
-const UsuarioDB = mongoose.model('Usuario', studentSchema);
+// Exportar uma instância única da classe
+const instance = new MongoDBConnection();
+Object.freeze(instance);
+
+module.exports = instance;
